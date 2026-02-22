db376d11123ecaac92043476bae61360a36bd9e9
Camiev04 <camiev2004@gmail.com>
2026-02-23 00:13:20 +0100

    1

---
+++ index-CEV.js
@@ -0,0 +1,20 @@
+
+const data  = [
+    { country : "Poland", year :2010, milex_total :19710.8, milex_gdp :1.84, milex_per_capita :231.2},
+    { country : "Poland", year :2020, milex_total : 17431.8, milex_gdp :2.26, milex_per_capita :358.9},
+    { country : "Morocco", year :2010, milex_total :3160.8, milex_gdp :3.39, milex_per_capita :98},
+    { country : "Morocco", year :2022, milex_total :4995, milex_gdp :3.81, milex_per_capita :132.7},
+    { country : "Cuba", year :2015, milex_total :118.4, milex_gdp :3.08, milex_per_capita :10.5},
+    { country : "Cuba", year :2018, milex_total :128.6, milex_gdp :2.88, milex_per_capita :11.4},
+    { country : "Australia", year :2015, milex_total :24046.2, milex_gdp :1.95, milex_per_capita :1011.7},
+    { country : "Australia", year :2022, milex_total :32445.3, milex_gdp :1.88, milex_per_capita :1244},
+    { country :"China", year :2010, milex_total :0, milex_gdp :1.73, milex_per_capita :78.3},
+    { country :"China", year : 2018, milex_total :1604.4, milex_gdp :1.67, milex_per_capita :164.2}
+
+];
+
+let morocco = data.filter(p => p.country==="Morocco");
+
+let media_milex_total = morocco.reduce((acum,i) => acum + i.milex_total, 0) / morocco.length;
+
+console.log("La media del milex total de Marruecos es de", media_milex_total);
