dde13ab4b1a6d97fbe039acc78bc30743a0d69a4
Camiev04 <camiev2004@gmail.com>
2026-02-28 18:26:48 +0100

    añadir ruta estática /about

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ app.get('/cool',(req, res) =>{
     res.send(cool())
 } )
 
+app.use(express.static(require('path').join(__dirname, 'public')));
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
 app.listen(3000, () => {
     console.log('server running on http://localhost:3000')
 })
