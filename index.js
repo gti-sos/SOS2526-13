6cbebbeb5b565c257c46fe07e7a4fa774919aa35
Camiev04 <115065151+Camiev04@users.noreply.github.com>
2026-02-28 18:43:15 +0100

    Reorder static file serving in index.js

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ app.get('/cool',(req, res) =>{
     res.send(cool())
 } )
 
-app.use(express.static(require('path').join(__dirname, 'public')));
-
 app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
+
+app.use(express.static(require('path').join(__dirname, 'public')));
+
+
 
 app.listen(3000, () => {
     console.log('server running on http://localhost:3000')
