074e40d3407190ffe26fa2e95ed634e872fddef6
Moraleda <pabmoralv@alum.us.es>
2026-03-20 18:32:47 +0100

    Actualizo /about

--- src/front/src/routes/About.svelte
+++ src/front/src/routes/About.svelte
@@ -1,31 +1,3 @@
-<style>
-  :global(body) {
-    font-family: Arial, sans-serif;
-    margin: 40px;
-    background-color: #f5f5f5;
-  }
-
-  h1, h2 {
-    color: #333;
-  }
-
-  section {
-    background: white;
-    padding: 20px;
-    margin-bottom: 20px;
-    border-radius: 10px;
-    box-shadow: 0 0 5px rgba(0,0,0,0.1);
-  }
-
-  a {
-    color: #0077cc;
-    text-decoration: none;
-  }
-
-  a:hover {
-    text-decoration: underline;
-  }
-</style>
 
 <h1>SOS2526-13</h1>
 
@@ -43,7 +15,7 @@
 <section>
   <h2>🌐 Frontends</h2>
   <ul>
-    <li><a href="URL_FRONTEND_1" target="_blank">Frontend Miembro 1</a></li>
+    <li><a href="https://sos2526-13.onrender.com/conflict-stats" target="_blank">Frontend de Pablo Moraleda Álvarez</a></li>
     <li><a href="URL_FRONTEND_2" target="_blank">Frontend Miembro 2</a></li>
     <li><a href="URL_FRONTEND_3" target="_blank">Frontend Miembro 3</a></li>
   </ul>
