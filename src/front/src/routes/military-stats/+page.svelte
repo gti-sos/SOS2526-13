86a452c3a4d2a54be922c91378460839daaa4ad5
Camiev04 <camiev2004@gmail.com>
2026-03-21 20:11:50 +0100

    borrado de un dato en concreto eslei

--- src/front/src/routes/military-stats/+page.svelte
+++ src/front/src/routes/military-stats/+page.svelte
@@ -5,7 +5,7 @@
   let pais= $state("");
   let año= $state("");
 
-
+     
 
   //CARGAR TODOS LOS DATOS
   async function loadMilitaryDataColeccion(){
@@ -13,9 +13,11 @@
         {
             method: "GET"
         });
-       mensaje = "Datos cargados correctamente";
-
-    }
+        if(res.ok){
+            await getMilitaryDataColeccion();
+            mensaje = "Datos cargados correctamente";
+        }
+  }
   //GET TODOS LOS DATOS
   async function getMilitaryDataColeccion(){
     const res = await fetch("http://localhost:3000/api/v1/military-stats",
@@ -54,6 +56,47 @@
     }
   }
 
+  //AÑADIR UN NUEVO DATO
+  async function addMilitaryData(){
+
+    if(actPais === '' || actAño === '' || actMilexTotal === '' || actMilexPerCapita === '' || actMilexGDP === ''){
+        mensaje = "Por favor, rellena todos los campos";
+        return;
+    }
+    const newDato = {
+        country: actPais.trim(),
+        year: parseInt(actAño),
+        milex_total: parseFloat(actMilexTotal),
+        milex_gdp: parseFloat(actMilexGDP),
+        milex_per_capita: parseFloat(actMilexPerCapita)
+    };
+    const res = await fetch("http://localhost:3000/api/v1/military-stats",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newDato)
+        }
+    );
+    if(res.status == 201){
+        mensaje = "Dato añadido correctamente";
+        actPais = '';
+        actAño = '';    
+        actMilexTotal = '';
+        actMilexPerCapita = '';
+        actMilexGDP = '';
+        await getMilitaryDataColeccion();
+
+    }else if(res.status == 409){
+        mensaje = "Recurso ya existente";
+    }else if(res.status == 404){
+        mensaje = "Dato no encontrado";
+  } else{
+        mensaje = "Error al añadir el dato" + res.status;
+    }
+  }
+
   //BORRAR UN DATO EN CONCRETO
   async function deleteMilitaryData(pais, año){
     const res = await fetch(`http://localhost:3000/api/v1/military-stats/${pais}/${año}`,
@@ -64,8 +107,11 @@
     );
     if(res.status == 404){
         mensaje = "Dato no encontrado";
-    }else{
+    }else if(res.ok){
         mensaje = "Dato borrado correctamente";
+        await getMilitaryDataColeccion();
+    }else{
+        mensaje = "Error al borrar el dato" + res.status;
     }
   }
 
@@ -83,6 +129,53 @@
 
 <h1>Military Stats</h1>
 
-<button on:click={loadMilitaryDataColeccion}>Cargar datos</button>
-<button on:click={getMilitaryDataColeccion}>Mostrar datos</button>
+<button onclick={loadMilitaryDataColeccion}>Cargar datos</button>
+<button onclick={getMilitaryDataColeccion}>Ver/Refrescar tabla</button>
+<button onclick={deleteMilitaryDataColeccion}>Borrar todos los datos</button>
+
+<div>
+    <h3>Añadir nuevo registro</h3>
+    <input placeholder="País" bind:value={actPais}>
+    <input placeholder="Año" bind:value={actAño}>
+    <input placeholder="Milex total" bind:value={actMilexTotal}>
+    <input placeholder="Milex per capita" bind:value={actMilexPerCapita}>
+    <input placeholder="Milex GDP" bind:value={actMilexGDP}>
+    <button onclick={() => addMilitaryData()}>Añadir</button>
+
+</div>
+
+<p>{mensaje}</p>
+
+{#if datos.length > 0}
+    <table>
+        <thead>
+            <tr>
+                <th>Country</th>
+                <th>Year</th>
+                <th>Milex total</th>
+                <th>Milex per capita</th>
+                <th>Milex GDP</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each datos as item}
+                <tr>
+                    <td>{item.country}</td>
+                    <td>{item.year}</td>
+                    <td>{item.milex_total}</td>
+                    <td>{item.milex_per_capita}</td>
+                    <td>{item.milex_gdp}</td>
+                    <td>
+                        <button onclick={() => abrirEditor(item)}>Editar</button>
+                    </td>
+                    <td>
+                        <button onclick={() => deleteMilitaryData(item.country, item.year)}>Borrar</button>
+                    </td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+{:else}
+    <p>Haz clic en "Ver Tabla" para mostrar los resultados.</p>
+{/if}
 
