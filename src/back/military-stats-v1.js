b2af88057b53313e8599605a41616b876956a731
Camila <camiev2004@gmail.com>
2026-04-04 15:43:19 +0200

    1

--- src/back/military-stats-v1.js
+++ src/back/military-stats-v1.js
@@ -1,6 +1,6 @@
 import dataStore from "nedb";
 
-const db = new Datastore({ filename: "military-stats-v1.db", autoload: true });
+const db = new dataStore({ filename: "military-stats-v1.db", autoload: true });
 
 let BASE_API_URL = "/api/v1/military-stats";
 
